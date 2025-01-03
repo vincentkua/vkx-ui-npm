2f6303dc7f5a431645ef6fc95c320cf981bbfe46
vincentkua <zylyn89@gmail.com>
2025-01-03 11:06:33 +0800

    fix: styled component for Container shall be placed out it

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -5,19 +5,18 @@ type Childprops = {
   children: React.ReactNode;
 };
 
-const Container = ({ children }: Childprops) => {
-  const StyledContainerDiv = styled.div`
-    max-width: 1200px;
-    margin: auto;
-    @media (max-width: 1200px) {
-      max-width: 1000px;
-    }
-    @media (max-width: 1000px) {
-      max-width: 100%;
-    }
-  `;
-  
+const StyledContainerDiv = styled.div`
+  max-width: 1200px;
+  margin: auto;
+  @media (max-width: 1200px) {
+    max-width: 1000px;
+  }
+  @media (max-width: 1000px) {
+    max-width: 100%;
+  }
+`;
 
+const Container = ({ children }: Childprops) => {
   return (
     <>
       <StyledContainerDiv>{children}</StyledContainerDiv>
